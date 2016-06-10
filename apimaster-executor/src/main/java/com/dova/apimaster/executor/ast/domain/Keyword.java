551d257cbaa23b65fb7bff33ce668cce015e78e2
dongeforever <zhendongliu92@yeah.net>
2016-06-10 12:48:33 +0800

    add keyword

---
+++ apimaster-executor/src/main/java/com/dova/apimaster/executor/ast/domain/Keyword.java
@@ -0,0 +1,13 @@
+package com.dova.apimaster.executor.ast.domain;
+
+/**
+ * Created by liuzhendong on 16/6/7.
+ */
+public class Keyword {
+
+    public final static String REQUEST = "request";
+    public final static String RESPONSE = "response";
+    public final static String BODY = "body";
+    public final static String PATH = "path";
+    public final static String HEADER = "header";
+}
---
+++ apimaster-web/src/main/java/com/dova/apimaster/web/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.dova.apimaster.web.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by liuzhendong on 16/6/10.
+ */
+@Controller
+public class HelloController {
+
+
+
+}
