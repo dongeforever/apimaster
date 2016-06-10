8efcbfa49f0ae84d79d685113c155aba755fb340
liuzhendong <liuzhendong@jiashuangkuaizi.com>
2016-06-10 15:02:12 +0800

    add static

--- apimaster-web/src/main/java/com/dova/apimaster/web/controller/HelloController.java
+++ apimaster-web/src/main/java/com/dova/apimaster/web/controller/HelloController.java
@@ -1,7 +1,15 @@
 package com.dova.apimaster.web.controller;
 
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * Created by liuzhendong on 16/6/10.
@@ -9,6 +17,18 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class HelloController {
 
+    @RequestMapping("hello")
+    public ResponseEntity hello(){
+        return ResponseEntity.ok("中国");
+    }
 
+    @RequestMapping("hello2")
+    public ResponseEntity hello2(){
+        return ResponseEntity.ok(Lists.newArrayList("中","国"));
+    }
 
+    @RequestMapping("hello3")
+    public String hello3(){
+        return "index";
+    }
 }
